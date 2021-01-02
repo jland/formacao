e3ecec293382789a821f623c157a1d1207784f3c
jland <jland25@gmail.com>
2021-01-02 18:14:48 +0000

    novo

---
+++ form/test.php
@@ -0,0 +1,17 @@
+<?PHP
+$sender = 'geral@jlandrade.com';
+$recipient = 'email@jlandrade.net';
+
+$subject = "php mail test";
+$message = "php test message";
+$headers = 'From:' . $sender;
+
+if (mail($recipient, $subject, $message, $headers))
+{
+    echo "Message accepted";
+}
+else
+{
+    echo "Error: Message not accepted";
+}
+?>
